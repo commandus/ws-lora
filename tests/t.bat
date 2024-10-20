0dd9e60f1d43dd6828b2bad04bb12f7243fd029c
andrei <andrei.i.ivanov@gmail.com>
2024-10-20 16:23:54 +0900

    Win compat

--- tests/t.bat
+++ tests/t.bat
@@ -1,3 +1,6 @@
 wget http://127.0.0.1:8050/json/clause -H 'Content-Type:application/json' -O- --post-file classes.json 2>2.txt
 wget http://127.0.0.1:8050/json/clause -H 'Content-Type:application/json' -O- --post-file keygen.json 2>2.txt
 wget http://127.0.0.1:8050/json/clause -H 'Content-Type:application/json' -O- --post-file netid.json 2>2.txt
+wget http://127.0.0.1:8050/json/clause -H 'Content-Type:application/json' -O- --post-file urn.json 2>2.txt
+wget http://127.0.0.1:8050/version -H 'Content-Type:application/json' -O- --post-data '' 2>2.txt
+wget http://127.0.0.1:8050/clause -H 'Content-Type:application/json' -O- --post-data '{}' 2>2.txt
